3ba005f69edee789f6683db07c33ba3b404d2b4c
bethanygreen <25246124+bethanygreen@users.noreply.github.com>
2022-06-29 11:09:34 -0400

    restyled

--- mario.c
+++ mario.c
@@ -3,47 +3,46 @@
 
 int main(void)
 {
-// Get user-inputted height
-int height = get_int("Height: ");
-// Only 1-8 integers
-while (height < 1 || height > 8)
-{
-    height = get_int("Height: ");
-}
-
-int counter = 0;
-
-while (height > 0)
-{
-    // Print spaces
-    int spaces = height - 1;
-    for (int i = spaces; i > 0; i--)
+    // Get user-inputted height
+    int height = get_int("Height: ");
+    // Only 1-8 integers
+    while (height < 1 || height > 8)
     {
-    printf(" ");
-    };
+        height = get_int("Height: ");
+    }
 
-    // Print left-side hashes
-    for (int i = 1; i < counter + 2; i++)
+    int counter = 0;
+
+    while (height > 0)
     {
-    printf("#");
-    };
+        // Print spaces
+        int spaces = height - 1;
+        for (int i = spaces; i > 0; i--)
+        {
+            printf(" ");
+        };
 
-    // Print middle spaces
-    printf("  ");
+        // Print left-side hashes
+        for (int i = 1; i < counter + 2; i++)
+        {
+            printf("#");
+        };
 
-    // Print right-side hashes
-    for (int i = 1; i < counter + 2; i++)
-    {
-    printf("#");
-    };
+        // Print middle spaces
+        printf("  ");
 
-    // New line
-    printf("\n");
+        // Print right-side hashes
+        for (int i = 1; i < counter + 2; i++)
+        {
+            printf("#");
+        };
 
-    // Increment counts
-    height--;
-    spaces--;
-    counter++;
-}
+        // New line
+        printf("\n");
 
+        // Increment counts
+        height--;
+        spaces--;
+        counter++;
+    }
 }
